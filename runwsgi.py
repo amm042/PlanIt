4a15c7fe2db2a973e9cdf195cf41df143806a29a
Alan Marchiori <amm042@bucknell.edu>
2016-11-29 14:38:02 -0700

    fixed logging

--- runwsgi.py
+++ runwsgi.py
@@ -19,8 +19,12 @@ logfile =  os.path.splitext(sys.argv[0])[0] + ".log"
 
 logging.basicConfig(
     level = logging.DEBUG,
-    handlers = (logging.StreamHandler(stream=sys.stdout),
-                logging.handlers.RotatingFileHandler(logfile,
+    handlers = (
+        logging.StreamHandler(stream=sys.stdout),
+              logging.handlers.RotatingFileHandler(
+                logfile,
+                maxBytes = 256*1024,
+                backupCount = 6)),
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 app = Flask('main')
