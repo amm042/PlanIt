3bfb4dd2dbbf26403750f43cb895f4e837f4a768
Alan Marchiori <amm042@bucknell.edu>
2017-02-21 10:35:59 -0700

    import app from planitwebservices file.

--- runwsgi.py
+++ runwsgi.py
@@ -25,47 +25,7 @@ from web.decorators import *
 # patch for gevent cooperative tasking
 monkey.patch_all()
 
-logfile = os.path.splitext(sys.argv[0])[0] + ".log"
-
-logging.basicConfig(
-    level = logging.DEBUG,
-    handlers = (
-        logging.StreamHandler(stream=sys.stdout),
-              logging.handlers.RotatingFileHandler(
-                logfile,
-                maxBytes = 256*1024,
-                backupCount = 6)),
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-app = Flask('main',static_url_path='/static')
-app.config.update({
-    'MONGO_HOST': 'eg-mongodb',
-    'MONGO_PORT': 27017,
-    'MONGO_DBNAME': 'planit',
-    'MONGO_USERNAME': 'webservice',
-    'MONGO_PASSWORD': 'F7ZGLY86xjby',
-})
-# key for session
-app.config['SECRET_KEY'] = "not a very secret key."
-app.config['APPLICATION_ROOT'] = '/planit'
-app.config['DEBUG'] = True
-app.config['EXPLAIN_TEMPLATE_LOADING'] = False
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-planitdb.init_db(app)
-
-app.register_blueprint(bp_login, url_prefix='/planit/user')
-app.register_blueprint(bp_keyapi, url_prefix='/planit/keys')
-app.register_blueprint(bp_cslpwan, url_prefix='/planit/cslpwan')
-app.register_blueprint(bp_planitapi, url_prefix='/planit/planitapi')
-app.register_blueprint(bp_planitv1, url_prefix='/planit/planit')
-app.register_blueprint(bp_docs, url_prefix='/planit/docs')
-app.register_blueprint(bp_root, url_prefix='/planit')
-
-# @app.errorhandler(404)
-# def page_not_found(error):
-#     print(error)
-#     print(request.url)
-#     return "page not found", 404
+from PlanItWebServices import app
 
 @app.before_request
 def redirect_eg():
