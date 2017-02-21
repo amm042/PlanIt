d146d0efeccd0c8f35a98c72a9d05229a9fa2fc2
Alan Marchiori <amm042@bucknell.edu>
2017-02-21 10:42:14 -0700

    added makeapp for consistent debug/production

--- PlanItWebServices.py
+++ PlanItWebServices.py
@@ -45,31 +45,33 @@ logging.basicConfig(level=logging.DEBUG,
                                                                 backupCount = 6), ),
                 format='%(asctime)s - [%(process)d]- %(name)s - %(levelname)s - %(message)s')
 
-app = Flask('main', static_url_path='/web/static')
-app.config.update({
-    'MONGO_HOST': 'eg-mongodb',
-    'MONGO_PORT': 27017,
-    'MONGO_DBNAME': 'planit',
-    'MONGO_USERNAME': 'webservice',
-    'MONGO_PASSWORD': 'F7ZGLY86xjby',
-})
-# key for session
-app.config['SECRET_KEY'] = "not a very secret key."
-#app.config['EXPLAIN_TEMPLATE_LOADING'] = True
-#mongo.init_app(app)
-planitdb.init_db(app)
+def makeApp(preprefix = '', debug=False):
+    app = Flask('main', static_url_path=preprefix+'/web/static')
+    app.config.update({
+        'MONGO_HOST': 'eg-mongodb',
+        'MONGO_PORT': 27017,
+        'MONGO_DBNAME': 'planit',
+        'MONGO_USERNAME': 'webservice',
+        'MONGO_PASSWORD': 'F7ZGLY86xjby',
+    })
+    # key for session
+    app.config['SECRET_KEY'] = "not a very secret key."
+    app.config['DEBUG'] = debug
+    #app.config['EXPLAIN_TEMPLATE_LOADING'] = True
+    #mongo.init_app(app)
+    planitdb.init_db(app)
 
-app.register_blueprint(bp_login, url_prefix='/user')
-app.register_blueprint(bp_keyapi, url_prefix='/keys')
-app.register_blueprint(bp_cslpwan, url_prefix='/cslpwan')
-app.register_blueprint(bp_planitapi, url_prefix='/planitapi')
-app.register_blueprint(bp_planitv1, url_prefix='/planit')
-app.register_blueprint(bp_docs, url_prefix='/docs')
-app.register_blueprint(bp_root, url_prefix='')
+    app.register_blueprint(bp_login, url_prefix=preprefix+'/user')
+    app.register_blueprint(bp_keyapi, url_prefix=preprefix+'/keys')
+    app.register_blueprint(bp_cslpwan, url_prefix=preprefix+'/cslpwan')
+    app.register_blueprint(bp_planitapi, url_prefix=preprefix+'/planitapi')
+    app.register_blueprint(bp_planitv1, url_prefix=preprefix+'/planit')
+    app.register_blueprint(bp_docs, url_prefix=preprefix+'/docs')
+    app.register_blueprint(bp_root, url_prefix=preprefix)
 
 
 if __name__=="__main__":
-
+    app = MakeApp(debug=True)
     app.run(host="0.0.0.0", port=8000, debug=True)
     #http_server = WSGIServer(('', 5000), app)
     #http_server.serve_forever()
--- runwsgi.py
+++ runwsgi.py
@@ -6,26 +6,57 @@ from urllib.parse import urlparse, urlunparse
 
 # logging
 import logging
-import logging.handlers
-import os
-import sys
-
-# blueprints
-from web.login import bp_login
-from web.keyapi import bp_keyapi
-from web.planit import bp_planitv1
-from web.planitapi import bp_planitapi
-from web.cslpwan import bp_cslpwan
-from web.root import bp_root
-from web.thedocs import bp_docs
-# from web import *
-from web.data import *
-from web.decorators import *
 
 # patch for gevent cooperative tasking
 monkey.patch_all()
 
-from PlanItWebServices import app
+from PlanItWebServices import makeApp
+"""
+OLD
+
+logfile = os.path.splitext(sys.argv[0])[0] + ".log"
+
+logging.basicConfig(
+    level = logging.DEBUG,
+    handlers = (
+        logging.StreamHandler(stream=sys.stdout),
+              logging.handlers.RotatingFileHandler(
+                logfile,
+                maxBytes = 256*1024,
+                backupCount = 6)),
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+app = Flask('main',static_url_path='/static')
+app.config.update({
+    'MONGO_HOST': 'eg-mongodb',
+    'MONGO_PORT': 27017,
+    'MONGO_DBNAME': 'planit',
+    'MONGO_USERNAME': 'webservice',
+    'MONGO_PASSWORD': 'F7ZGLY86xjby',
+})
+# key for session
+app.config['SECRET_KEY'] = "not a very secret key."
+app.config['APPLICATION_ROOT'] = '/planit'
+app.config['DEBUG'] = True
+app.config['EXPLAIN_TEMPLATE_LOADING'] = False
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+planitdb.init_db(app)
+
+app.register_blueprint(bp_login, url_prefix='/planit/user')
+app.register_blueprint(bp_keyapi, url_prefix='/planit/keys')
+app.register_blueprint(bp_cslpwan, url_prefix='/planit/cslpwan')
+app.register_blueprint(bp_planitapi, url_prefix='/planit/planitapi')
+app.register_blueprint(bp_planitv1, url_prefix='/planit/planit')
+app.register_blueprint(bp_docs, url_prefix='/planit/docs')
+app.register_blueprint(bp_root, url_prefix='/planit')
+"""
+
+
+# @app.errorhandler(404)
+# def page_not_found(error):
+#     print(error)
+#     print(request.url)
+#     return "page not found", 404
 
 @app.before_request
 def redirect_eg():
@@ -49,5 +80,7 @@ def redirect_eg():
     logging.info("redirect {} to {}".format(request.url, x))
     return redirect(x, code=301)
 
+
+app = makeApp(preprefix='/planit')
 http_server = WSGIServer(('', 4002), app)
 http_server.serve_forever()
